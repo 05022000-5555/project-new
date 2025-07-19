e6cc441a84dab29c0563047a83aec9f3738ac75a
dvhung2 <daohung2000vp@gmail.com>
2025-07-20 01:23:27 +0700

    init project

---
+++ script.js
@@ -0,0 +1,14 @@
+// Tạo trái tim rơi
+function createHeart() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+  heart.style.left = Math.random() * 100 + 'vw';
+  heart.style.animationDuration = 2 + Math.random() * 3 + 's';
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
+
+setInterval(createHeart, 300);
